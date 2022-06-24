087235b328b87421c67f838abffef71c5a436a78
WuliMengX <wulimengx@outlook.com>
2022-06-24 22:34:08 +0800

    IDEA

--- JAVA/JavaLearning/src/App.java
+++ JAVA/JavaLearning/src/App.java
@@ -1,5 +1,12 @@
 public class App {
+
+    public static final double PI = 3.14;
+    static final int y = 10;
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        final double x = 3.3;
+        System.out.println(PI*y*x);
+
     }
 }
