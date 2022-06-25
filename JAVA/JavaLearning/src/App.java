e7dc0b482a0e1f79fd99beff95b060d54021d95b
WuliMengX <wulimengx@outlook.com>
2022-06-26 02:35:30 +0800

    IDEA

--- JAVA/JavaLearning/src/App.java
+++ JAVA/JavaLearning/src/App.java
@@ -1,5 +1,8 @@
+/**
+ * @author wulim
+ */
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
