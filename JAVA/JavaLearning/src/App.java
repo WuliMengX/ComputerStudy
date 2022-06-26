13a70360e253a32c65bba61e461a311dfeca2440
WuliMengX <wulimengx@outlook.com>
2022-06-26 21:59:43 +0800

    IDEA

--- JAVA/JavaLearning/src/App.java
+++ JAVA/JavaLearning/src/App.java
@@ -1,8 +1,8 @@
 /**
- * @author wulim
+ * @author Liujungang
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello World!");
     }
 }
