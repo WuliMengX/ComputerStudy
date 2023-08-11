d0d0a3170559553cefb1a351a85b2a203d3fb039
WuliMengX <wulimengx@Outlook.com>
2023-08-12 04:41:21 +0800

    test

--- MacOS/Cplusplus/helloworld.cpp
+++ MacOS/Cplusplus/helloworld.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
-using namespace std;
+/*
+ 1.Comment
+ 2.Warnings and Errors
+ 3.
+
+*/
+
+
 
 int main() {
-    cout << "Hello, World!" << endl;
+    
+    int firstNumber = 12;
+    int secondNumber = 3;
+
+    int sum = firstNumber + secondNumber;
+    std::cout << sum << std::endl;
     return 0;
 }
