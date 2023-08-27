bea05d4d326cfebefb9d4966d01f8e7e1b01f3ad
WuliMengX <wulimengx@outlook.com>
2023-08-27 11:45:12 +0800

    MacOS

---
+++ MacOS/MacCpp/MacCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MacCpp
+//
+//  Created by Learning Deep on 2023/8/27.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
